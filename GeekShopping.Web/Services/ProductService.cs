37da8bf618b1343d637743bc95b8e00915fb8c23
Luiz Renato <lrenato12@outlook.com.br>
2024-11-22 12:46:16 -0300

    Updated Projetc

--- GeekShopping.Web/Services/ProductService.cs
+++ GeekShopping.Web/Services/ProductService.cs
@@ -1,32 +1,57 @@
 ﻿using GeekShopping.Web.Models;
 using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Utils;
 
 namespace GeekShopping.Web.Services;
 
 public class ProductService : IProductService
 {
-    public async Task<ProductModel> CreateProduct(ProductModel requestModel)
-    {
-        throw new NotImplementedException();
-    }
+    private readonly HttpClient _client;
+    public const string BASE_PATH = "api/v1/Product";
 
-    public async Task<bool> DeleteProductById(long id)
+    public ProductService()
     {
-        throw new NotImplementedException();
+        
     }
 
     public async Task<IEnumerable<ProductModel>> FindAllProducts()
     {
-        throw new NotImplementedException();
+        var response = await _client.GetAsync(BASE_PATH);
+
+        return await response.ReadContentAsync<List<ProductModel>>();
     }
 
     public async Task<ProductModel> FindProductById(long id)
     {
-        throw new NotImplementedException();
+        var response = await _client.GetAsync($"{BASE_PATH}/{id}");
+
+        return await response.ReadContentAsync<ProductModel>();
+    }
+
+    public async Task<ProductModel> CreateProduct(ProductModel requestModel)
+    {
+        var response = await _client.PostAsJson(BASE_PATH, requestModel);
+
+        if (response.IsSuccessStatusCode)
+            return await response.ReadContentAsync<ProductModel>();
+
+        throw new Exception("Something went wrong when calling API");
     }
 
     public async Task<ProductModel> UpdateProduct(ProductModel requestModel)
     {
-        throw new NotImplementedException();
+        var response = await _client.PutAsJson(BASE_PATH, requestModel);
+
+        if (response.IsSuccessStatusCode)
+            return await response.ReadContentAsync<ProductModel>();
+
+        throw new Exception("Something went wrong when calling API");
+    }
+
+    public async Task<bool> DeleteProductById(long id)
+    {
+        var response = await _client.DeleteAsync($"{BASE_PATH}/{id}");
+
+        return await response.ReadContentAsync<bool>();
     }
 }
